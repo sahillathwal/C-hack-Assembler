7820307a6f553c86c79cba610cea59003812af77
sahillathwal <sahillathwal566@gmail.com>
2024-03-19 15:45:06 +0530

    adding hasMoreCommands function to main.c

--- main.c
+++ main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-// input file to store the file arrgument provided by user
+// varabile to store the file arrgument provided by user
 char *input_file;
 
 // FILE pointer to store the file pointer created by initializer function
 FILE *file_pointer;
 
 int is_ok = EXIT_FAILURE;
+
 // function declerations
 int initializer(char *);
+bool hasMoreCommands();
 
 int main(int arg, char *vargs[])
 {
@@ -18,19 +21,35 @@ int main(int arg, char *vargs[])
 
     // calling initializer function to open the file for parsing
     initializer(input_file);
-    int c;                                   // note: int, not char, required to handle EOF
-    while ((c = fgetc(file_pointer)) != EOF) // standard C I/O file reading loop
-        putchar(c);
+
+    // checking by printing the content of the input file
+    while (hasMoreCommands() == true)
+    {
+        char buf[100] = {0};
+        fgets(buf, sizeof buf, file_pointer);
+        printf("%s", buf);
+    }
 }
 
 // Opens the input file/stream for parsing and stores the file pointer to file_pointer.
 int initializer(char *input_file)
 {
     file_pointer = fopen(input_file, "r");
+
     // check if file opening failed
     if (!file_pointer)
     {
         perror("File opening failed");
         return is_ok;
     }
+}
+
+// function to check if there are more commands left in the input file
+bool hasMoreCommands()
+{
+    // if the end of the stream has not been reached feof returns zero; hence ture_false gets assgined false
+    bool ture_false = (feof(file_pointer) == 0) ? true : false;
+
+    // returns false value if end of file has been reached else returns ture
+    return ture_false;
 }
