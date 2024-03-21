a352bcc6b241560ca7a8cd1589881c0d4a35568d
sahillathwal <sahillathwal566@gmail.com>
2024-03-21 17:39:20 +0530

    updating parser.h to write global variables needed and including statmeant to include stdlib.h

--- parser.h
+++ parser.h
@@ -1,7 +1,22 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdlib.h>
 #include <stdbool.h>
+
+// varabile to store the file arrgument provided by user
+char *input_file;
+
+// FILE pointer to store the file pointer created by initializer function
+FILE *file_pointer;
+
+// varaible to store cuurent command
+char *current_command;
+
+// varaible to store command type
+char *command_type;
+
+int is_ok = EXIT_FAILURE;
 
 int initializerParser(char *);
 bool hasMoreCommandsParser();
