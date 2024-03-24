00c8bef3e3b9b0965cb8b4552dbe34dd5b935839
sahillathwal <sahillathwal566@gmail.com>
2024-03-24 21:16:47 +0530

    updating hashtable.h with mini hashtable funcationality declarations

---
+++ hashtable.h
@@ -0,0 +1,26 @@
+// hashtable.h
+
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include <stdlib.h>
+#include <string.h>
+// Define a structure for the hash table entry
+typedef struct
+{
+    char *key;
+    char *value;
+} HashEntry;
+
+// Define a structure for the hash table itself
+typedef struct
+{
+    HashEntry **entries;
+} HashTable;
+
+// Function prototypes
+HashTable *createHashTable();
+void insert(HashTable *hashTable, const char *key, char *value);
+char *get(HashTable *hashTable, const char *key);
+
+#endif // HASHTABLE_H
