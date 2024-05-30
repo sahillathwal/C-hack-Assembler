ce9ae7f183d7a8906984567af92cea053a5e12bf
sahillathwal <sahillathwal566@gmail.com>
2024-05-30 12:53:30 +0530

    updating hashtable.h to accomadate changes for code module

--- hashtable.h
+++ hashtable.h
@@ -6,24 +6,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define the size of the hash table in main.c
-extern int TABLE_SIZE;
-
 // Define a structure for the hash table entry
-typedef struct
+typedef struct HashEntry
 {
     char *key;
     char *value;
+    struct HashEntry *next;
 } HashEntry;
 
 // Define a structure for the hash table itself
 typedef struct
 {
+    int size;
     HashEntry **entries;
 } HashTable;
 
 // Function prototypes
-HashTable *createHashTable();
+HashTable *createHashTable(int size);
 void insert(HashTable *hashTable, const char *key, char *value);
 char *get(HashTable *hashTable, const char *key);
 
